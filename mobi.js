103556cd5a837f4b037844d4fcf2c073cb5a5f73
kesavagit <chennajk1991@gmail.com>
2021-08-02 20:13:23 +0530

    copied one file

---
+++ mobi.js
@@ -0,0 +1,10 @@
+<html>
+<body>
+<h1> Home Page </h1>
+<p> Chennakesava Jakkala,
+fghnumutngbfvdcs
+please vfguhijmedk
+please subscribe chennakesava jakkala
+this line is added in bitbucket </p>
+</body>
+</html>
